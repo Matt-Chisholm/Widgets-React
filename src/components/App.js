3c8199750df1c360584ed5c0607aefaa079250fc
Matt Chisholm <mathesonchisholm@hotmail.com>
2022-04-19 12:09:56 -0700

    started dropdown component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Accordion from "./Accordion";
 import Search from "./Search";
+import Dropdown from "./Dropdown";
 
 export default function App() {
   const items = [
@@ -18,11 +19,27 @@ export default function App() {
     },
   ];
 
+  const options = [
+    {
+      label: 'The Color Red',
+      value: 'red'
+    },
+    {
+      label: 'The Color Green',
+      value: 'green'
+    },
+    {
+      label: 'The Color Blue',
+      value: 'blue'
+    }
+  ];
+
   return (
     <div>
       <br />
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <Dropdown options={options} />
     </div>
   );
 }
