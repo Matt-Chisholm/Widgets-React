73811924568ca2dbf943fcc3b50ddb8a738cc03c
Matt Chisholm <mathesonchisholm@hotmail.com>
2022-04-19 12:46:18 -0700

    refactored closing drop down

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-export default function Dropdown({ options, selected, onSelectedChange }) {
+export default function Dropdown ({ options, selected, onSelectedChange }) {
+
   const [open, setOpen] = useState(false);
 
+
+  useEffect(() => {
+    document.body.addEventListener(
+      "click",
+      () => {
+        setOpen(false);
+      },
+      { capture: true }
+    );
+  }, []);
+
+  
   const renderOptions = options.map((option) => {
     if (option.value === selected.value) {
       return null;
@@ -25,10 +38,15 @@ export default function Dropdown({ options, selected, onSelectedChange }) {
     <div className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
-        <div className={`ui selection dropdown ${open ? 'visible active' : ''}`} onClick={() => setOpen(!open)}>
+        <div
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+          onClick={() => setOpen(!open)}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className={`menu ${open ? 'visible transition' : ''}`}>{renderOptions}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderOptions}
+          </div>
         </div>
       </div>
     </div>
