9b0a8e8dd41f2f12c2ebf7470d98b4ea61094a1c
Matt Chisholm <mathesonchisholm@hotmail.com>
2022-04-19 11:48:57 -0700

    search component/playing with hooks

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,68 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Search() {
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+
+    const timeoutID = setTimeout(() => {
+      if (term) {
+        search();
+      }
+    }, 800);
+
+    return () => {
+      clearTimeout(timeoutID);
+    };
+  }, [term]);
+
+  const renderResults = results.map((result) => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className="ui button"
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
+
   return (
-    <div>Search</div>
-  )
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+      <div className="ui celled list">{renderResults}</div>
+    </div>
+  );
 }
